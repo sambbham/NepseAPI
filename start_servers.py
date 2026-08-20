5483f3d8a281254c2ebe3f550a9f44d1e41f8095
sambbham <sambbham@proton.me>
2026-08-20 22:18:40 +0545

    Change default port from 8000 to 10000

--- start_servers.py
+++ start_servers.py
@@ -4,7 +4,7 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "10000"))
 
     print(f"Starting application on port {port}...")
 
